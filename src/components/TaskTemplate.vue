a805312bcc37756d03627973034b7222c19c738a
luckycod3r <94208991+luckycod3r@users.noreply.github.com>
2023-03-15 03:36:38 +1100

    adding dist subtree

---
+++ src/components/TaskTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TaskTemplate"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/tasks/ToDoList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "ToDoList"
+}
+</script>
+
+<style scoped>
+
+</style>
